614c9d04194f747deb4c46fd5ab4aaf93f73f73d
Shivam Vishwakarma <shivam92211@gmail.com>
2023-07-24 23:00:06 +0530

    images added

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -37,7 +37,7 @@ const HeroBanner = () => {
             >
                 <div>
                     <img
-                        src="/assets/img4.jpg"
+                        src="/assets/img1.jpg"
                         className="aspect-[16/10] md:aspect-auto object-cover"
                     />
                     <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
@@ -47,7 +47,17 @@ const HeroBanner = () => {
 
                 <div>
                     <img
-                        src="/assets/img5.jpg"
+                        src="/assets/img2.jpg"
+                        className="aspect-[16/10] md:aspect-auto object-cover"
+                    />
+                    <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
+                        Shop now
+                    </div>
+                </div>
+
+                <div>
+                    <img
+                        src="/assets/img3.jpg"
                         className="aspect-[16/10] md:aspect-auto object-cover"
                     />
                     <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
